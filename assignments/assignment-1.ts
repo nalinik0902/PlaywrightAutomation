c0a7adaa60d4bdf88342937910407eb5d90deef6
Nalini <nalinik0902@gmail.com>
2026-03-10 06:58:15 -0400

    Assignment1

---
+++ assignments/assignment-1.ts
@@ -0,0 +1,45 @@
+// 1. Temperature of a city in degree celsius 25.5 
+      let degreeOFTemperature:number=25.5;
+
+ // 2. Wheather customer has placed an order True/False
+      let oredrStatus:boolean=true;
+ // 3. Person's phone number :"123-456-7890"
+      let personPh_Number:string="123-456-7890";
+ // 4. Amount of money in bank account : 1000.50
+      let account_balance:number=1000.50;
+ // 5. Person's email address :"john.doe@example.com"
+      let personEmailAddress:string="john.doe@example.com";
+ // 6.  Coordinates 
+        let latitude:number=37.7749;
+        let longitude:number=-122.4194;
+ // 7. Marital Status
+       let marital_status:boolean=true;
+ // 8.  Ocuupation
+     let occupation:string="software Engineer";
+ // 9. favourite color
+        let color:string="Blue";
+ // 10. current year
+        let year:number=2023;
+  // 11.  Followers
+        let Followers:number=1000000;
+  // 12. Movie rating
+        let movieRating:number=7.5;
+  // 13. BloodType
+        let bloodType:string="A";
+  // 14. Book title
+      let bookTitle:string="To killa a mockingbird"  ;
+  // 15. Number of employees
+       let employee:number=500;
+  // 16. Event time
+      let  eventTime:number|string="2:30 PM";
+  // 17. Country name
+     let countryName:string="UnitedStates";
+  // 18. person's eye color
+     let eyeColor:string="Brown";
+  // 19. person's birthplace
+     let birthPlace:string="New york city";
+  // 20. Distance between two cities
+     let distance:number=200.5;
+                                         
+                              
+
